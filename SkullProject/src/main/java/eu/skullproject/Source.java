7cc5444254d33580aeca06cf9b65950fca6b9be0
AlolanRaichu <thedestinypig@gmail.com>
2016-08-14 20:21:35 +0100

    Update Source.java

--- SkullProject/src/main/java/eu/skullproject/Source.java
+++ SkullProject/src/main/java/eu/skullproject/Source.java
@@ -6,5 +6,5 @@ public class Source {
 	  public static final String modid = "Barra";
 	  public static final String mod_VersionName = "BETA";
 	  public static final String mcVersion = "1.8.9";
-	  
+	  public static final String logTag = "[SKULL] ";
 }
