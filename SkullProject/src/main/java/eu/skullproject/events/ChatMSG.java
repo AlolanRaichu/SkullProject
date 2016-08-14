098e1d3322d2cb62df8c6356cfb8c3fd4c886318
CoalCoding <coalores@gmail.com>
2016-08-14 20:25:13 +0100

    Update ChatMSG.java

--- SkullProject/src/main/java/eu/skullproject/events/ChatMSG.java
+++ SkullProject/src/main/java/eu/skullproject/events/ChatMSG.java
@@ -3,7 +3,20 @@ package eu.skullproject.events;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 
 public class ChatMSG {
+    public static boolean isAFK = false;
+	public static String reason = "";
+    
+    @SubscribeEvent
     public void onChatMSG(ClientChatReceivedEvent e) {
+        String message = e.message.getUnformattedText();
+		if (message.contains("From") && isAFK) {
+			Minecraft.getMinecraft().thePlayer.sendChatMessage("/r I am currently AFK. Reason: " + ChatMSG.reason);
+		}
+	
+
+			
+
+		
     
     }
 }
